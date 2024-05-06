91aadd14842a4b4a2b0f4ab09f7d8a86115212e3
tofayeLL <ahmedtofayel132@gmail.com>
2024-05-06 14:17:05 +0600

    use custom hooks for axios and create base url and creadential default

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,29 +1,34 @@
-// import { useEffect, useState } from "react";
-import useServices from "../../../hooks/useServices";
+import { useEffect, useState } from "react";
+// import useServices from "../../../hooks/useServices";
 import ServiceCard from "./ServiceCard";
 // import axios from "axios";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 
 const Services = () => {
     // user custom hooks
-    const services = useServices();
-    console.log(services);
+    /*  const services = useServices();
+     console.log(services); */
 
 
-    /*  const [services, setServices] = useState([]);
-     
-     
- 
-     useEffect(() => {
- 
-         // use axios
-         axios.get('http://localhost:5000/services',  { withCredentials: true })
-             .then(data => {
-                 setServices(data.data);
- 
-             })
-  */
+    const [services, setServices] = useState([]);
+    
+
+    // custom hook for axios
+    const axiosSecure = useAxiosSecure();
+
+
+    useEffect(() => {
+
+        // use axios
+        axiosSecure.get('/services')
+            .then(data => {
+                setServices(data.data);
+
+            })
+    }, [])
+
 
     //  use  normal fetch
 
@@ -33,24 +38,24 @@ const Services = () => {
          console.log(data);
          setServices(data);
      }) */
-// }, [])
+    // }, [])
 
 
-return (
-    <div>
-        <h1>services:{services.length}</h1>
-        <div className="grid lg:grid-cols-3 grid-cols-1 gap-5">
-            {
-                services.map(service => <ServiceCard
-                    service={service}
-                    key={service._id}>
+    return (
+        <div>
+            <h1>services:{services.length}</h1>
+            <div className="grid lg:grid-cols-3 grid-cols-1 gap-5">
+                {
+                    services.map(service => <ServiceCard
+                        service={service}
+                        key={service._id}>
 
-                </ServiceCard>)
-            }
+                    </ServiceCard>)
+                }
+            </div>
+
         </div>
-
-    </div>
-);
+    );
 };
 
 export default Services;
